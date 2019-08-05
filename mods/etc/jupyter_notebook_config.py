e6f479282e2c940376ccee3d94ff621a1c0c9463
Daniel Rosskopp <info@upcal.de>
2019-08-05 11:31:03 +0200

    Added configs and run command

---
+++ mods/etc/jupyter_notebook_config.py
@@ -0,0 +1,10 @@
+import os
+#os.environ["R_LIBS_USER"]=os.environ["HOME"]+"/.R/%V/R_LIBS_USER"
+os.environ["PYTHONUSERBASE"]=os.environ["HOME"]+"/.jupyterhub/1.0.0/"
+os.environ["JUPYTER_RUNTIME_DIR"]=os.environ["HOME"]+"/.jupyterhub/1.0.0/jupyter/run"
+os.environ["JUPYTER_DATA_DIR"]=os.environ["HOME"]+"/.jupyterhub/1.0.0/jupyter/data"
+#c.NotebookApp.contents_manager_class = "jupytext.TextFileContentsManager"
+c.NotebookApp.contents_manager_class = "jupytext.TextFileContentsManager"
+c.ContentsManager.default_jupytext_formats = "ipynb,py"
+c.ContentsManager.preferred_jupytext_formats_save = "py:light"
+c.ContentsManager.default_cell_markers = "{{{,}}}"
