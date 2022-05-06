66f00d6f71331d52728e16d84fe3374869b19457
Daniel Rosskopp <drosskopp@upcal.de>
2022-05-06 09:51:13 +0200

    New Version

---
+++ 4.0.1/mods/jupyter_notebook_config.py
@@ -0,0 +1,12 @@
+import os
+#os.environ["R_LIBS_USER"]=os.environ["HOME"]+"/.R/%V/R_LIBS_USER"
+if not os.path.isdir(os.environ["HOME"]+"/.jupyterhub/python/"):
+    os.makedirs(os.environ["HOME"]+"/.jupyterhub/python/")
+os.environ["PYTHONUSERBASE"]=os.environ["HOME"]+"/.jupyterhub/python/"
+os.environ["JUPYTER_RUNTIME_DIR"]=os.environ["HOME"]+"/.jupyterhub/python/jupyter/run"
+os.environ["JUPYTER_DATA_DIR"]=os.environ["HOME"]+"/.jupyterhub/python/jupyter/data"
+#c.NotebookApp.contents_manager_class = "jupytext.TextFileContentsManager"
+c.NotebookApp.contents_manager_class = "jupytext.TextFileContentsManager"
+c.ContentsManager.default_jupytext_formats = "ipynb,py"
+c.ContentsManager.preferred_jupytext_formats_save = "py:light"
+c.ContentsManager.default_cell_markers = "{{{,}}}"
